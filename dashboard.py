4b186d6570ba65352cb4c034a5f35fd0123d7f09
Jon <jon@Jons-MacBook-Pro.local>
2020-03-20 20:34:30 -0400

    configured for heroku deploy

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,10 @@ import requests
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
 # Grab JH data and make dataframe
 url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series" \
       "/time_series_19-covid-Confirmed.csv"
