f0a8321bed30c1836dba0443e8e746a00a5e28a2
Jon <jon@Jons-MacBook-Pro.local>
2020-03-22 15:15:54 -0400

    layout now reloads on page refesh instead of server start

--- dataplot.py
+++ dataplot.py
@@ -1,29 +1,23 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
+import io
+import requests
 
-df = pd.read_csv('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv').T
-headers = df.iloc[1]
-new_df = pd.DataFrame(df.values[0:], columns=headers)
-new_df = new_df.drop(new_df.index[1])
-#print(new_df.head())
+url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series" \
+      "/time_series_19-covid-Confirmed.csv"
+s = requests.get(url).content
+df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+df.columns = df.columns.str.strip().str.lower().str.replace('/', '_')
 
-df_US = new_df.loc[:, 'US']
-print(df_US.columns[df_US.iloc[0,:].isin(['New York'])])
 
-# df[df.columns[df.isin([0,1]).all()]]
+df = df.loc[df['country_region'] == 'US']
 
-#y_mass = new_df.loc[3:,'Massachusetts']
-#y_conn = new_df.loc[3:,'Connecticut']
-#y_ny = new_df.loc[3:,'New York']
-#y_hub = new_df.loc[3:, 'Hubei']
-y_ity = new_df.loc[3:, 'US']
+Total = df.groupby['3_21_20'].sum()
 
-#print(new_df.loc[:, 'US'].head())
+print(df.head())
 
-fig = go.Figure([go.Scatter(y=y_ity, name='Italy')])
-#fig.add_trace(go.Scatter(y=y_conn, name='Connecticut'))
-#fig.add_trace(go.Scatter(y=y_ny, name='New York'))
-#fig.add_trace(go.Scatter(y=y_ity, name='Italy'))
+#new_df = df.loc[['US']]
+# new_df = df.loc[df['column_name'] == some_value]
+# print(new_df)
 
-#fig.show()
+#df_US = new_df.loc[:, 'US']
+#print(df_US.columns[df_US.iloc[0,:].isin(['New York'])])
