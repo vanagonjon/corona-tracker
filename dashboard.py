a024efa874d4e6074d96f49d3632793d3eefc1de
Jon <jon@Jons-MacBook-Pro.local>
2020-03-28 15:03:15 -0400

    data frame loads in hidden div, top 15 countrys are plotted by default

--- dashboard.py
+++ dashboard.py
@@ -9,30 +9,6 @@ import requests
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# Grab JH data and make dataframe
-#url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series" \
-#      "/time_series_covid19_confirmed_global.csv"
-url = "https://raw.githubusercontent.com/vanagonjon/corona-tracker/timeshift/time_series_19-covid-Confirmed.csv"
-s = requests.get(url).content
-df = pd.read_csv(io.StringIO(s.decode('utf-8')))
-# Clean column names
-df.columns = df.columns.str.strip().str.lower().str.replace('/', '_')
-
-# Convert states to string, and remove nan values
-df['province_state'] = df['province_state'].astype(str).str.replace('nan', '')
-# Concatenate state and country names in a list
-locations = df[['province_state', 'country_region']].agg(', '.join, axis=1).values.tolist()
-locations = [x.strip(', ') for x in locations]
-
-top_index = df.iloc[:,-1].sort_values(ascending=False).index.tolist()[1:20]
-
-states = ['Massachusetts','New York','California','Washington','Louisiana','Florida','New Jersy']
-states_index = df.loc[df['province_state'].isin(states)].index.tolist()
-
-print(states_index)
-print(type(states_index))
-#rprint(df.iloc[373].head()
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
@@ -40,14 +16,18 @@ server = app.server
 
 def serve_layout():
     site = html.Div([
+        html.Div(id='setup', style={'display': 'none'}),
+
+        html.Div(id='diagnosed_cache', style={'display': 'none'}),
+
         dcc.Dropdown(
             id='locationsdd',
-            options=[{'label': locations[i], 'value': i} for i in df.index.tolist()],
-            value=top_index,
             multi=True,
         ),
-        #dcc.Graph(id='main_graph'),
+
+        # dcc.Graph(id='main_graph'),
         html.Div(id='dd-output-container'),
+
         dcc.RadioItems(
             options=[
                 {'label': 'Linear', 'value': 'linear'},
@@ -67,27 +47,34 @@ app.layout = serve_layout
 @app.callback(
     Output('dd-output-container', 'children'),
     [Input('locationsdd', 'value'),
-     Input('linlog', 'value')])
-def plot_data(location_list, y_axis_type):
-    if location_list is None:
+     Input('locationsdd', 'options'),
+     Input('linlog', 'value'),
+     Input('diagnosed_cache', 'children')])
+def plot_data(location_index, location_options, y_axis_type, cached_data):
+    if cached_data is None:
+        raise dash.exceptions.PreventUpdate
+
+    df = pd.read_json(cached_data)
+
+    if location_index is None:
         return {}
     else:
         try:
             fig_data = []
-            for location in location_list:
+            for location in location_index:
                 fig_data.append(
                     go.Scatter(
                         y=df.iloc[[location], 4:].values[0, :],
                         x=df.iloc[[location], :].columns[4:],
-                        name=locations[location],
+                        name=location_options[location].get('label'),
                         opacity=0.8,
                     )
                 )
         except TypeError:
             fig_data = [go.Scatter(
-                y=df.iloc[[location_list], 4:].values[0, :],
-                x=df.iloc[[location_list], :].columns[4:],
-                name=locations[location_list],
+                y=df.iloc[[location_options], 4:].values[0, :],
+                x=df.iloc[[location_options], :].columns[4:],
+                name=location_options[location].get('label'),
                 opacity=0.8
             )]
 
@@ -95,9 +82,9 @@ def plot_data(location_list, y_axis_type):
         xaxis={'type': 'category', 'title': "Date"},
         yaxis={'type': 'linear', 'title': "Total Cases Diagnosed"},
         yaxis_type=y_axis_type,
-        # margin={'l': 60, 'b': 40, 'r': 10, 't': 10},
+        margin={'l': 60, 'b': 40, 'r': 10, 't': 10},
     )
-    figure = dcc.Graph(figure = go.Figure(
+    figure = dcc.Graph(figure=go.Figure(
         data=fig_data,
         layout=layout))
 
@@ -118,9 +105,35 @@ def plot_data(location_list, y_axis_type):
     children = [
         html.Div(id='test', children=[
             figure,
-            ])
-        ]
+        ])
+    ]
     return children
+
+
+# Called on page load, imports data from web and creates dataframe, options for drop down, and default selections to
+# include in plot
+@app.callback([Output('diagnosed_cache', 'children'),
+               Output('locationsdd', 'options'),
+               Output('locationsdd', 'value')],
+              [Input('setup', 'children')])
+def setup(foo):
+    url = "https://raw.githubusercontent.com/vanagonjon/corona-tracker/timeshift/time_series_19-covid-Confirmed.csv"
+    s = requests.get(url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    # Clean column names
+    df.columns = df.columns.str.strip().str.lower().str.replace('/', '_')
+
+    # Convert states to string, and remove nan values
+    df['province_state'] = df['province_state'].astype(str).str.replace('nan', '')
+
+    locations = df[['province_state', 'country_region']].agg(', '.join, axis=1).values.tolist()
+    locations = [x.strip(', ') for x in locations]
+
+    location_options = [{'label': locations[i], 'value': i} for i in df.index.tolist()]
+
+    top_index = df.iloc[:, -1].sort_values(ascending=False).index.tolist()[1:20]
+
+    return df.to_json(), location_options, top_index
 
 
 if __name__ == '__main__':
