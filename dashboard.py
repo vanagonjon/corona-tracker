6c30c7f8131a7bd28c5ccf33d85406b8e03e0142
Jon <jon@Jons-MacBook-Pro.local>
2020-04-03 00:04:59 -0400

    using server side flask cashing

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,8 @@
+import datetime as dt
+
+import numpy as np
+from flask_caching import Cache
+
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
@@ -10,33 +15,54 @@ import requests
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+cache = Cache(app.server, config={
+    'CACHE_TYPE': 'filesystem',
+    'CACHE_DIR': 'cache-directory'
+})
 
 server = app.server
+
+TIMEOUT = 600
+
+
+@cache.memoize(timeout=TIMEOUT)
+def query_data():
+    url = "https://raw.githubusercontent.com/vanagonjon/corona-tracker/master/time_series_19-covid-Confirmed.csv"
+    s = requests.get(url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    # Clean column names
+    df.columns = df.columns.str.strip().str.lower().str.replace('/', '_')
+
+    # Convert states to string, and remove nan values
+    df['province_state'] = df['province_state'].astype(str).str.replace('nan', '')
+
+    return df.to_json(date_format='iso', orient='split')
+
+
+def dataframe():
+    return pd.read_json(query_data(), orient='split')
 
 
 def server_layout():
     site = html.Div([
+
         html.Div(id='setup', style={'display': 'none'}),  # Hidden div, loads data frames, and drop down data
 
-        html.Div(id='diagnosed_cache', style={'display': 'none'}),  # Hidden div, caches data frame
+        dcc.Dropdown( id='locationsdd',multi=True),
 
-        dcc.Dropdown(
-            id='locationsdd',
-            multi=True,
-        ),
-
-        # dcc.Graph(id='main_graph'),
-        html.Div(id='dd-output-container'),
-
-        dcc.RadioItems(
-            options=[
-                {'label': 'Linear', 'value': 'linear'},
-                {'label': 'Log', 'value': 'log'}
-            ],
-            value='log',
-            labelStyle={'display': 'inline-block'},
-            id='linlog'
-        ),
+        html.Div(id='dd-output-container',
+                 children=[
+                     dcc.Graph(id='cases'),
+                     dcc.RadioItems(
+                         options=[
+                             {'label': 'Linear', 'value': 'linear'},
+                             {'label': 'Log', 'value': 'log'}
+                         ],
+                         value='log',
+                         labelStyle={'display': 'inline-block'},
+                         id='linlog'
+                     ),
+        ]),
     ])
     return site
 
@@ -45,16 +71,12 @@ app.layout = server_layout
 
 
 @app.callback(
-    Output('dd-output-container', 'children'),
+    Output('cases', 'figure'),
     [Input('locationsdd', 'value'),
      Input('locationsdd', 'options'),
-     Input('linlog', 'value'),
-     Input('diagnosed_cache', 'children')])
-def plot_data(location_index, location_options, y_axis_type, cached_data):
-    if cached_data is None:
-        raise dash.exceptions.PreventUpdate
-
-    df = pd.read_json(cached_data)
+     Input('linlog', 'value')])
+def plot_data(location_index, location_options, y_axis_type):
+    df = dataframe()
 
     if location_index is None:
         return {}
@@ -82,49 +104,18 @@ def plot_data(location_index, location_options, y_axis_type, cached_data):
         xaxis={'type': 'category', 'title': "Date"},
         yaxis={'type': 'linear', 'title': "Total Cases Diagnosed"},
         yaxis_type=y_axis_type,
-        margin={'l': 60, 'b': 40, 'r': 10, 't': 10},
+        margin={'l': 60, 'b': 100, 'r': 10, 't': 30},
     )
-    figure = dcc.Graph(figure=go.Figure(
-        data=fig_data,
-        layout=layout))
+    figure={'data':fig_data,'layout':layout}
 
-    # slides = dcc.Slider(
-    #     min=0,
-    #     max=10,
-    #     step=None,
-    #     marks={
-    #         0: '0 °F',
-    #         3: '3 °F',
-    #         5: '5 °F',
-    #         7.65: '7.65 °F',
-    #         10: '10 °F'
-    #     },
-    #     value=5
-    # )
-
-    children = [
-        html.Div(id='test', children=[
-            figure,
-        ])
-    ]
-    return children
+    return figure
 
 
-# Called on page load, imports data from web and creates dataframe, options for drop down, and default selections to
-# include in plot
-@app.callback([Output('diagnosed_cache', 'children'),
-               Output('locationsdd', 'options'),
+@app.callback([Output('locationsdd', 'options'),
                Output('locationsdd', 'value')],
               [Input('setup', 'children')])
 def setup(foo):
-    url = "https://raw.githubusercontent.com/vanagonjon/corona-tracker/master/time_series_19-covid-Confirmed.csv"
-    s = requests.get(url).content
-    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
-    # Clean column names
-    df.columns = df.columns.str.strip().str.lower().str.replace('/', '_')
-
-    # Convert states to string, and remove nan values
-    df['province_state'] = df['province_state'].astype(str).str.replace('nan', '')
+    df = dataframe()
 
     locations = df[['province_state', 'country_region']].agg(', '.join, axis=1).values.tolist()
     locations = [x.strip(', ') for x in locations]
@@ -133,7 +124,7 @@ def setup(foo):
 
     top_index = df.iloc[:, -1].sort_values(ascending=False).index.tolist()[0:15]
 
-    return df.to_json(), location_options, top_index
+    return location_options, top_index
 
 
 if __name__ == '__main__':
--- memoizationtestexample.py
+++ memoizationtestexample.py
@@ -1,14 +1,16 @@
 import datetime as dt
-import os
-import time
+
+import numpy as np
+from flask_caching import Cache
 
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-import numpy as np
-import pandas as pd
+import dash_core_components as dcc
 from dash.dependencies import Input, Output
-from flask_caching import Cache
+import plotly.graph_objs as go
+import pandas as pd
+import io
+import requests
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -18,73 +20,111 @@ cache = Cache(app.server, config={
     'CACHE_DIR': 'cache-directory'
 })
 
-TIMEOUT = 60
+server = app.server
+
+TIMEOUT = 600
+
 
 @cache.memoize(timeout=TIMEOUT)
 def query_data():
-    # This could be an expensive data querying step
-    df =  pd.DataFrame(
-        np.random.randint(0,100,size=(100, 4)),
-        columns=list('ABCD')
-    )
-    now = dt.datetime.now()
-    df['time'] = [now - dt.timedelta(seconds=5*i) for i in range(100)]
+    url = "https://raw.githubusercontent.com/vanagonjon/corona-tracker/master/time_series_19-covid-Confirmed.csv"
+    s = requests.get(url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    # Clean column names
+    df.columns = df.columns.str.strip().str.lower().str.replace('/', '_')
+
+    # Convert states to string, and remove nan values
+    df['province_state'] = df['province_state'].astype(str).str.replace('nan', '')
+
     return df.to_json(date_format='iso', orient='split')
 
 
 def dataframe():
     return pd.read_json(query_data(), orient='split')
 
-app.layout = html.Div([
-    html.Div('Data was updated within the last {} seconds'.format(TIMEOUT)),
-    dcc.Dropdown(
-        id='live-dropdown',
-        value='A',
-        options=[{'label': i, 'value': i} for i in dataframe().columns]
-    ),
-    dcc.Graph(id='live-graph')
-])
+
+def server_layout():
+    site = html.Div([
+
+        html.Div(id='setup', style={'display': 'none'}),  # Hidden div, loads data frames, and drop down data
+
+        dcc.Dropdown( id='locationsdd',multi=True),
+
+        html.Div(id='dd-output-container',
+                 children=[
+                     dcc.Graph(id='cases'),
+                     dcc.RadioItems(
+                         options=[
+                             {'label': 'Linear', 'value': 'linear'},
+                             {'label': 'Log', 'value': 'log'}
+                         ],
+                         value='log',
+                         labelStyle={'display': 'inline-block'},
+                         id='linlog'
+                     ),
+        ]),
+    ])
+    return site
 
 
-@app.callback(Output('live-graph', 'figure'),
-              [Input('live-dropdown', 'value')])
-def update_live_graph(value):
+app.layout = server_layout
+
+
+@app.callback(
+    Output('cases', 'figure'),
+    [Input('locationsdd', 'value'),
+     Input('locationsdd', 'options'),
+     Input('linlog', 'value')])
+def plot_data(location_index, location_options, y_axis_type):
     df = dataframe()
-    now = dt.datetime.now()
-    return {
-        'data': [{
-            'x': df['time'],
-            'y': df[value],
-            'line': {
-                'width': 1,
-                'color': '#0074D9',
-                'shape': 'spline'
-            }
-        }],
-        'layout': {
-            # display the current position of now
-            # this line will be between 0 and 60 seconds
-            # away from the last datapoint
-            'shapes': [{
-                'type': 'line',
-                'xref': 'x', 'x0': now, 'x1': now,
-                'yref': 'paper', 'y0': 0, 'y1': 1,
-                'line': {'color': 'darkgrey', 'width': 1}
-            }],
-            'annotations': [{
-                'showarrow': False,
-                'xref': 'x', 'x': now, 'xanchor': 'right',
-                'yref': 'paper', 'y': 0.95, 'yanchor': 'top',
-                'text': 'Current time ({}:{}:{})'.format(
-                    now.hour, now.minute, now.second),
-                'bgcolor': 'rgba(255, 255, 255, 0.8)'
-            }],
-            # aesthetic options
-            'margin': {'l': 40, 'b': 40, 'r': 20, 't': 10},
-            'xaxis': {'showgrid': False, 'zeroline': False},
-            'yaxis': {'showgrid': False, 'zeroline': False}
-        }
-    }
+
+    if location_index is None:
+        return {}
+    else:
+        try:
+            fig_data = []
+            for location in location_index:
+                fig_data.append(
+                    go.Scatter(
+                        y=df.iloc[[location], 4:].values[0, :],
+                        x=df.iloc[[location], :].columns[4:],
+                        name=location_options[location].get('label'),
+                        opacity=0.8,
+                    )
+                )
+        except TypeError:
+            fig_data = [go.Scatter(
+                y=df.iloc[[location_options], 4:].values[0, :],
+                x=df.iloc[[location_options], :].columns[4:],
+                name=location_options[location].get('label'),
+                opacity=0.8
+            )]
+
+    layout = go.Layout(
+        xaxis={'type': 'category', 'title': "Date"},
+        yaxis={'type': 'linear', 'title': "Total Cases Diagnosed"},
+        yaxis_type=y_axis_type,
+        margin={'l': 60, 'b': 100, 'r': 10, 't': 30},
+    )
+    figure={'data':fig_data,'layout':layout}
+
+    return figure
+
+
+@app.callback([Output('locationsdd', 'options'),
+               Output('locationsdd', 'value')],
+              [Input('setup', 'children')])
+def setup(foo):
+    df = dataframe()
+
+    locations = df[['province_state', 'country_region']].agg(', '.join, axis=1).values.tolist()
+    locations = [x.strip(', ') for x in locations]
+
+    location_options = [{'label': locations[i], 'value': i} for i in df.index.tolist()]
+
+    top_index = df.iloc[:, -1].sort_values(ascending=False).index.tolist()[0:15]
+
+    return location_options, top_index
 
 
 if __name__ == '__main__':
